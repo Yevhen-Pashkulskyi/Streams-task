07aac7a85e34456b2c4b5079ffb3df50d54ea42d
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-02-25 15:21:56 +0200

    version_02

--- src/app/controller/Chat.java
+++ src/app/controller/Chat.java
@@ -10,24 +10,24 @@ public class Chat {
     private List<User> users;
 
     public Chat() {
-       users = new ArrayList<>();
+        users = new ArrayList<>();
     }
 
     public void addUser(User user) {
-        users .add(user);
+        users.add(user);
     }
 
-    public List<User> filterUsers() {
-        return users.stream().filter(user -> user.getEmailUser().contains("123"))
+    public List<User> filterUsers(String filter) {
+        return users.stream().filter(user -> user.getEmailUser().contains(filter))
                 .collect(Collectors.toList());
     }
 
-    public void printFilterUsers() {
-        List<User> filteredUsers = filterUsers();
+    public void printFilterUsers(String filter) {
+        List<User> filteredUsers = filterUsers(filter);
         if (filteredUsers.isEmpty()) {
-            System.out.println("Немає користувачів, які мають в е-пошті '123'");
+            System.out.println("Немає користувачів, які мають в е-пошті " + filter);
         } else {
-            System.out.println("Коритсувачі, які мають в е-пошті '123': "
+            System.out.println("Коритсувачі, які мають в е-пошті " + filter + ": "
                     + filteredUsers.size());
             for (User user : filteredUsers) {
                 System.out.println(user);
--- src/app/controller/CreateUsers.java
+++ src/app/controller/CreateUsers.java
@@ -3,16 +3,31 @@ package app.controller;
 import app.model.User;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class CreateUsers {
-    public void run(){
-        Chat chat = new Chat();
-        chat.addUser(new User("Anna", "anna@gmail.com"));
-        chat.addUser(new User("Bogdan", "bogdan@gmail.com"));
-        chat.addUser(new User("Yevhen", "yevhen123@gmail.com"));
-        chat.addUser(new User("Victor", "victor@gmail.com"));
-        chat.addUser(new User("Oleg", "oleg123@gmail.com"));
+    private final int MAX_USERS = 5;
 
-        chat.printFilterUsers();
+    public void run() {
+        Chat chat = new Chat();
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < MAX_USERS; i++) {
+            System.out.println("Введіть ім'я " + (i + 1) + "-го користувача: ");
+            String name = scanner.nextLine();
+            System.out.println("Введіть е-пошту " + (i + 1) + "-го коритсувача: ");
+            String email = scanner.nextLine();
+            chat.addUser(new User(name, email));
+        }
+//        chat.addUser(new User("Anna", "anna@gmail.com"));
+//        chat.addUser(new User("Bogdan", "bogdan@gmail.com"));
+//        chat.addUser(new User("Yevhen", "yevhen123@gmail.com"));
+//        chat.addUser(new User("Victor", "victor@gmail.com"));
+//        chat.addUser(new User("Oleg", "oleg123@gmail.com"));
+        System.out.println("Пошук користувачів з однаковим параметром \n"
+                + "Введіть параметр : ");
+        String filter = scanner.nextLine();
+        scanner.close();
+        chat.printFilterUsers(filter);
     }
 }
