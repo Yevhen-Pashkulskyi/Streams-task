d1b62ae6136bf684361ab4d46e6875be151447bc
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-02-25 15:36:15 +0200

    corrected version_02

--- src/app/controller/CreateUsers.java
+++ src/app/controller/CreateUsers.java
@@ -13,17 +13,12 @@ public class CreateUsers {
         Scanner scanner = new Scanner(System.in);
         int count = 0;
         for (int i = 0; i < MAX_USERS; i++) {
-            System.out.println("Введіть ім'я " + (count + 1) + "-го користувача: ");
+            System.out.println("Введіть ім'я " + (i + 1) + "-го користувача: ");
             String name = scanner.nextLine();
-            System.out.println("Введіть е-пошту " + (count + 1) + "-го коритсувача: ");
+            System.out.println("Введіть е-пошту " + (i + 1) + "-го коритсувача: ");
             String email = scanner.nextLine();
             chat.addUser(new User(name, email));
         }
-//        chat.addUser(new User("Anna", "anna@gmail.com"));
-//        chat.addUser(new User("Bogdan", "bogdan@gmail.com"));
-//        chat.addUser(new User("Yevhen", "yevhen123@gmail.com"));
-//        chat.addUser(new User("Victor", "victor@gmail.com"));
-//        chat.addUser(new User("Oleg", "oleg123@gmail.com"));
         System.out.println("Пошук користувачів з однаковим параметром \n"
                 + "Введіть параметр : ");
         String filter = scanner.nextLine();
